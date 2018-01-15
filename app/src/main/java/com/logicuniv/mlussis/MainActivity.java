bce08465f74614e819eb8bafb73ec21566386635
joelqwh <33408852+joelqwh@users.noreply.github.com>
2018-01-15 14:47:06 +0800

    Initial commit

---
+++ app/src/main/java/com/logicuniv/mlussis/MainActivity.java
@@ -0,0 +1,13 @@
+package com.logicuniv.mlussis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
