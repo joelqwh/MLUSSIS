f9787e0514fc70a061403ae4fd1c97ffb1358d2b
joelqwh <33408852+joelqwh@users.noreply.github.com>
2018-01-18 18:36:06 +0800

    continued View Disbursement Use Case, left Pin
    Signed-off-by: joelqwh <33408852+joelqwh@users.noreply.github.com>

--- app/src/main/java/com/logicuniv/mlussis/PinDisbursementFragment.java
+++ app/src/main/java/com/logicuniv/mlussis/PinDisbursementFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,16 @@ public class PinDisbursementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pin_disbursement, container, false);
+        View v = inflater.inflate(R.layout.fragment_pin_disbursement, container, false);
+        Disbursement d = Disbursement.getCurrentDisbursementForDepartment("1");     //get parameter from login details when it is set up
+        if (d != null) {
+            TextView tv_Dept = v.findViewById(R.id.textView_deptRep_PinNumber);
+            //getDepartmentName(d.get("DeptCode"));     //to initialise this method later
+            tv_Dept.setText(d.get("Pin"));
+
+        }
+
+        return v;
     }
 
 }
