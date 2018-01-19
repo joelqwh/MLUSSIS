ad3ecf749912292a4f0f5ea90b3c988d1ce0cd38
collau <collau@hotmail.com>
2018-01-19 20:54:11 +0800

    Menu Page for Store Clerk

--- app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_MainActivity.java
+++ app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_MainActivity.java
@@ -3,8 +3,11 @@ package com.logicuniv.mlussis.StoreClerk;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
 
 import com.logicuniv.mlussis.Catalogue_EmployeeActivity;
 import com.logicuniv.mlussis.DisbursementActivity;
@@ -13,11 +16,16 @@ import com.logicuniv.mlussis.R;
 
 public class StoreClerk_MainActivity extends Activity {
 
+    GridLayout storeClerkMain;
+
     // The Main Screen probably the login screen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_storeclerk);
+
+        storeClerkMain = (GridLayout)findViewById(R.id.storeClerkMainGrid);
+        setSingleClickEvent(storeClerkMain);
     }
 
 
@@ -41,6 +49,28 @@ public class StoreClerk_MainActivity extends Activity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setSingleClickEvent (GridLayout mainGrid) {
+        for(int i=0; i<mainGrid.getChildCount();i++)
+        {
+            CardView cardView = (CardView)mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view){
+                    if (finalI==0)
+                    {
+                        startActivity(new Intent(getApplicationContext(), InventoryActivity.class));
+                    }
+                    if (finalI==1)
+                    {
+                        startActivity(new Intent(getApplicationContext(), DisbursementActivity.class));
+                    }
+                }
+            });
         }
     }
 }
