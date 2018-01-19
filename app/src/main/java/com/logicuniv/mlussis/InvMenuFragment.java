aeebdc374ac14fa25daa3bbb4e0477df074372df
collau <collau@hotmail.com>
2018-01-19 11:07:36 +0800

    InventoryActivity Files

---
+++ app/src/main/java/com/logicuniv/mlussis/InvMenuFragment.java
@@ -0,0 +1,28 @@
+package com.logicuniv.mlussis;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvMenuFragment extends Fragment {
+
+
+    public InvMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inventory_skeleton,container,false);
+    }
+
+}
