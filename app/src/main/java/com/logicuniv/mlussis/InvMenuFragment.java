84d8fc04483d5d117344db288fb2232122cb87ca
collau <collau@hotmail.com>
2018-01-19 17:50:21 +0800

    Fragment code changes for StoreClerk Inventory Screen

--- app/src/main/java/com/logicuniv/mlussis/InvMenuFragment.java
+++ app/src/main/java/com/logicuniv/mlussis/InvMenuFragment.java
@@ -1,28 +1,50 @@
 package com.logicuniv.mlussis;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InvMenuFragment extends Fragment {
 
+    ArrayList<StationeryCatalogue> arraystationery = StationeryCatalogue.getCatalogue();
 
     public InvMenuFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inventory_skeleton,container,false);
+        View v = inflater.inflate(R.layout.fragment_inventory_skeleton,container,false);
+        display (arraystationery);
+
+        return v;
+    }
+
+    void display(ArrayList<StationeryCatalogue> details) {
+        final String TAG = "INVTABLE_FRAG";
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction trans = fm.beginTransaction();
+
+        Fragment frag = new InvTableFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("stationerycatalogue", details);
+        frag.setArguments(args);
+        if (fm.findFragmentByTag(TAG)==null)
+            trans.add(R.id.invDetailsFragment, frag, TAG);
+        else
+            trans.replace(R.id.invDetailsFragment, frag, TAG);
+        trans.commit();
     }
 
 }
--- app/src/main/java/com/logicuniv/mlussis/InventoryActivity.java
+++ app/src/main/java/com/logicuniv/mlussis/InventoryActivity.java
@@ -13,5 +13,6 @@ public class InventoryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
     }
 }
