23388927bb0d79c43bbeccb97414a01bf49f395d
roman015 <xaviervarricatt@live.in>
2018-01-26 13:41:34 +0800

    You can't use back button on login screen now.

--- app/src/main/java/com/logicuniv/mlussis/Backend/App.java
+++ app/src/main/java/com/logicuniv/mlussis/Backend/App.java
@@ -3,6 +3,9 @@ package com.logicuniv.mlussis.Backend;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
 
 /**
  * Created by xavie on 24-01-2018.
@@ -24,5 +27,27 @@ public class App extends Application {
 
     public static Context getAppContext() {
         return appContext;
+    }
+
+    public static boolean checkConnection() {
+        boolean result = false;
+        String response;
+
+        try {
+            response = JSONParser.postStream(
+                    App.WCFServer + "test",
+                    "").trim();
+
+            if (response.equals("test"))
+            {
+                result = true;
+            }
+        } catch (Exception e)
+        {
+            Log.e("CheckConnection", e.getMessage());
+            result = false;
+        }
+
+        return result;
     }
 }
--- app/src/main/java/com/logicuniv/mlussis/LoginActivity.java
+++ app/src/main/java/com/logicuniv/mlussis/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.logicuniv.mlussis;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -15,6 +16,15 @@ public class LoginActivity extends Activity {
 
     EditText usernameEditText;
     EditText passwordEditText;
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +43,14 @@ public class LoginActivity extends Activity {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                if(username.isEmpty() || password.isEmpty())
-                {
+                if (username.isEmpty() || password.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Please Fill in Both Fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                if(!LoginController.AuthenticateCredentials(getApplicationContext(),username, password))
-                {
+                if (!LoginController.AuthenticateCredentials(getApplicationContext(), username, password)) {
                     Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
+                } else {
                     // TODO : Redirect User to the first Page Here
                     Toast.makeText(getApplicationContext(), "Congrats!", Toast.LENGTH_LONG).show();
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
