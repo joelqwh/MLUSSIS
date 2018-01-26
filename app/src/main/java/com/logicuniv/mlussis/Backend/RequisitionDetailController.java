0b7db39154cb12e5d569c5647503e702806383d2
collau <collau@hotmail.com>
2018-01-26 19:28:24 +0800

    added addRequisitionDetail in RequisitionDetailController

--- app/src/main/java/com/logicuniv/mlussis/Backend/RequisitionDetailController.java
+++ app/src/main/java/com/logicuniv/mlussis/Backend/RequisitionDetailController.java
@@ -1,6 +1,12 @@
 package com.logicuniv.mlussis.Backend;
 
+import android.util.Log;
+
+import com.logicuniv.mlussis.Model.DisbursementDetail;
 import com.logicuniv.mlussis.Model.RequisitionDetail;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -10,35 +16,85 @@ import java.util.ArrayList;
 
 public class RequisitionDetailController {
 
-    static ArrayList<ArrayList<RequisitionDetail>> alalrd = new ArrayList<>();
-
-    public static void addRequisitionDetail (ArrayList<RequisitionDetail> alrd)
-    {
-        alalrd.add(alrd);
-//        Log.e("joel",alalrd.toString());;
-    }
-
     public static ArrayList<RequisitionDetail> getRequisitionDetail(String rnum)
     {
-//        RequisitionDetail reqA = new RequisitionDetail("R1","I001","2");
-//        RequisitionDetail reqB = new RequisitionDetail("R1","I002","3");
-//        ArrayList<RequisitionDetail> alrrrrd = new ArrayList<>();
-//        alrrrrd.add(reqA);
-//        alrrrrd.add(reqB);
-//        addRequisitionDetail(alrrrrd);      //just for dummy purposes
+        ArrayList<RequisitionDetail> result = new ArrayList<>();
 
-        ArrayList<RequisitionDetail> pack = new ArrayList<>();
-        for (ArrayList<RequisitionDetail> alrd:alalrd)
-        {
-            for (RequisitionDetail det :alrd)
-            {
-                if (det.get("ReqNo").equals(rnum))
-                {
-                    pack.add(det);
-                }
+        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonResult;
+
+        try {
+            jsonObject.put("sessionID", LoginController.getSessionID(App.getAppContext()));
+            jsonObject.put("ReqNo", rnum);
+
+            JSONArray jsonArray = new JSONArray (JSONParser.postStream(App.WCFServer + "RequisitionDetails", jsonObject.toString()));
+
+            for (int i=0; i<jsonArray.length(); i++){
+                JSONObject requisitionDetail = jsonArray.getJSONObject(i);
+                result.add(new RequisitionDetail(
+                        requisitionDetail.getString("ReqNo"),
+                        requisitionDetail.getString("ItemNo"),
+                        requisitionDetail.getString("Qty")
+                ));
             }
         }
-        return pack;
+        catch (Exception e)
+        {
+            Log.e("RequisitionDetail", e.getMessage());
+        }
+
+        return result;
+    }
+
+    public static boolean addRequisitionDetails (RequisitionDetail[] rdItems)
+    {
+        boolean result = false;
+
+        for(int i = 0; i < rdItems.length; i++)
+        {
+            if(!addRequisitionDetail(rdItems[i]))
+            {
+                result = false;
+                break;
+            }
+            else
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private static boolean addRequisitionDetail (RequisitionDetail rdItem)
+    {
+        Boolean result = false;
+
+        JSONObject jsonObject = new JSONObject();
+        JSONObject jsonRequisitionDetail = new JSONObject();
+        String response;
+
+        try {
+            //put requisition details into a JSONObject "jsonRequisitionDetail"
+            jsonRequisitionDetail.put("ReqNo", rdItem.get("ReqNo"));
+            jsonRequisitionDetail.put("ItemNo", rdItem.get("ItemNo"));
+            jsonRequisitionDetail.put("Qty", rdItem.get("Qty"));
+
+            //put JSONObject "jsonRequisitionDetail" and sessionID into a JSONObject "jsonObject"
+            jsonObject.put("sessionID", LoginController.getSessionID((App.getAppContext())));
+            jsonObject.put("addRequisitionDetail", jsonRequisitionDetail.toString());
+
+            //result of passing the "jsonObject".toString() into the WCF Server
+            response = JSONParser.postStream(App.WCFServer + "AddRequisitionDetail", jsonObject.toString());
+
+            result = response.trim().equals("true");
+        }
+        catch (Exception e)
+        {
+            Log.e("AddRequisitionDetail", e.getMessage());
+            result = false;
+        }
+
+        return result;
     }
 
     public void removeRequisitionDetail(RequisitionDetail rd){
@@ -51,3 +107,11 @@ public class RequisitionDetailController {
     }
 
 }
+
+
+//        RequisitionDetail reqA = new RequisitionDetail("R1","I001","2");
+//        RequisitionDetail reqB = new RequisitionDetail("R1","I002","3");
+//        ArrayList<RequisitionDetail> alrrrrd = new ArrayList<>();
+//        alrrrrd.add(reqA);
+//        alrrrrd.add(reqB);
+//        addRequisitionDetail(alrrrrd);      //just for dummy purposes
