562f0ccaafda90c67354dfd50a96c3fc96160120
roman015 <xaviervarricatt@live.in>
2018-01-29 16:23:32 +0800

    Added Extra Description Field for Disbursement Details

--- app/src/main/java/com/logicuniv/mlussis/Backend/DisbursementDetailController.java
+++ app/src/main/java/com/logicuniv/mlussis/Backend/DisbursementDetailController.java
@@ -84,6 +84,7 @@ public class DisbursementDetailController {
                 result.add(new DisbursementDetail(
                         jsonResult.getJSONObject(i).getString("DisbursementNo"),
                         jsonResult.getJSONObject(i).getString("ItemNo"),
+                        jsonResult.getJSONObject(i).getString("Description"),
                         jsonResult.getJSONObject(i).getString("Needed"),
                         jsonResult.getJSONObject(i).getString("Promised"),
                         jsonResult.getJSONObject(i).getString("Received")));
