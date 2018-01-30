3d5bb9ac03aac5661ba2560b67986d0022e8291f
roman015 <xaviervarricatt@live.in>
2018-01-30 21:04:29 +0800

    Added AdjustmentVoucherController

---
+++ app/src/main/java/com/logicuniv/mlussis/Backend/AdjustmentVoucherController.java
@@ -0,0 +1,30 @@
+package com.logicuniv.mlussis.Backend;
+
+import org.json.JSONObject;
+
+/**
+ * Created by xavie on 30-01-2018.
+ */
+
+public class AdjustmentVoucherController {
+    public static boolean createAdjustmentVoucher(String itemNo, int newQuantity, String reason) {
+        boolean result = false;
+        String response = "";
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put("sessionID", LoginController.getSessionID(App.getAppContext()));
+            jsonObject.put("ItemNo", itemNo);
+            jsonObject.put("Qty", newQuantity);
+            jsonObject.put("Reason", itemNo);
+
+            response = JSONParser.postStream(App.WCFServer + "CreateAdjustmentVoucher", jsonObject.toString());
+
+            result = response.contains("true");
+        } catch (Exception e) {
+            result = false;
+        }
+
+        return result;
+    }
+}
