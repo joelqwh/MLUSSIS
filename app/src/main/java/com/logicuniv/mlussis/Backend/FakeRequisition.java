c83476b2d10bc9a96c7cecc2ef5c91588b3f7f76
roman015 <xaviervarricatt@live.in>
2018-01-30 19:05:29 +0800

    Fake Requisition Controller.

---
+++ app/src/main/java/com/logicuniv/mlussis/Backend/FakeRequisition.java
@@ -0,0 +1,30 @@
+package com.logicuniv.mlussis.Backend;
+
+import com.logicuniv.mlussis.Model.Requisition;
+import com.logicuniv.mlussis.Model.RequisitionDetail;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ * Created by xavie on 30-01-2018.
+ */
+
+public class FakeRequisition {
+    public static ArrayList<RequisitionDetail> details = new ArrayList<>();
+    public static Requisition requisition = new Requisition();
+
+    public static void startNewRequisition(String empWhoIssued)
+    {
+        details.clear();
+        requisition = new Requisition(empWhoIssued, "");
+    }
+
+    public static void submitNewRequisition()
+    {
+        String reqNo = RequisitionController.CreateNewRequisition();
+        for (RequisitionDetail detail: details) {
+         RequisitionDetailController.addRequisitionDetail(detail);
+        }
+    }
+}
