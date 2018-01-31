a2707d9da7d0ed10c63c8010aea17a56ad892ec5
collau <collau@hotmail.com>
2018-01-31 10:26:21 +0800

    StockCardActivity and EditStockQtyActivity updated

--- app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_EditStockQtyActivity.java
+++ app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_EditStockQtyActivity.java
@@ -1,6 +1,7 @@
 package com.logicuniv.mlussis.StoreClerk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.logicuniv.mlussis.Backend.AdjustmentVoucherController;
+import com.logicuniv.mlussis.InventoryActivity;
 import com.logicuniv.mlussis.R;
 
 import java.util.HashMap;
@@ -20,21 +23,36 @@ public class StoreClerk_EditStockQtyActivity extends Activity {
         setContentView(R.layout.activity_store_clerk__edit_stock_qty);
         Bundle b = getIntent().getExtras();
         HashMap<String, String> sc = (HashMap<String, String>) b.get("invdetails");
-        TextView textEditStockItemCode = (TextView) findViewById(R.id.textStockCardItemCode);
+
+
+        final TextView textEditStockItemCode = (TextView) findViewById(R.id.textStockCardItemCode);
+        final TextView textEditStockItemDesc = (TextView) findViewById(R.id.textStockCardItemDesc);
+        final TextView textEditStockItemCurrentQty = (TextView) findViewById(R.id.textStockCardItemCurrentQty);
+        final EditText EditStockItemActualQty = (EditText) findViewById(R.id.textStockCardItemActualQty);
+        final EditText adjReason = (EditText) findViewById(R.id.editTextAdjReason);
+
         textEditStockItemCode.setText(sc.get("ItemNo"));
-        TextView textEditStockItemDesc = (TextView) findViewById(R.id.textStockCardItemDesc);
         textEditStockItemDesc.setText(sc.get("Description"));
+        textEditStockItemCurrentQty.setText(sc.get("CurrentQty"));
 
         Button itemAdjSubmitBtn = (Button) findViewById(R.id.itemAdjSubmitbtn);
         itemAdjSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText adjReason = (EditText) findViewById(R.id.editTextAdjReason);
+
                 if (adjReason.getText().toString().matches("")) {
                     Toast.makeText(StoreClerk_EditStockQtyActivity.this, "Please enter adjustment reason", Toast.LENGTH_LONG).show();
                     return;
                 } else {
+                    AdjustmentVoucherController.createAdjustmentVoucher
+                            (
+                            textEditStockItemCode.getText().toString(),
+                            Integer.parseInt(EditStockItemActualQty.getText().toString()),
+                            adjReason.getText().toString()
+                            );
                     Toast.makeText(StoreClerk_EditStockQtyActivity.this, "Submitted for Adjustment", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), InventoryActivity.class);
+                    startActivity(intent);
                 }
             }
         });
--- app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_StockCardActivity.java
+++ app/src/main/java/com/logicuniv/mlussis/StoreClerk/StoreClerk_StockCardActivity.java
@@ -23,20 +23,21 @@ public class StoreClerk_StockCardActivity extends Activity {
         setContentView(R.layout.activity_store_clerk__stock_card_);
         Bundle b = getIntent().getExtras();
         HashMap<String, String> sc = (HashMap<String, String>) b.get("invdetails1");
-        TextView t1 = findViewById(R.id.textViewItemCode);
-        t1.setText(sc.get("ItemNo"));
-        TextView t2 = findViewById(R.id.textViewItemDesc);
-        t2.setText(sc.get("Description"));
-        TextView t3 = findViewById(R.id.textViewItemBin);
-        t3.setText(sc.get("BinNo"));
-        TextView t4 = findViewById(R.id.textViewItemUOM);
-        t4.setText(sc.get("Uom"));
-        TextView t5 = findViewById(R.id.textViewItemSupp1);
-        t5.setText(sc.get("Supplier1"));
-        TextView t6 = findViewById(R.id.textViewItemSupp2);
-        t6.setText(sc.get("Supplier2"));
-        TextView t7 = findViewById(R.id.textViewItemSupp3);
-        t7.setText(sc.get("Supplier3"));
+        final TextView itemCode = findViewById(R.id.textViewItemCode);
+        TextView itemDesc = findViewById(R.id.textViewItemDesc);
+        TextView itemBin = findViewById(R.id.textViewItemBin);
+        TextView itemUOM = findViewById(R.id.textViewItemUOM);
+        TextView itemSupp1 = findViewById(R.id.textViewItemSupp1);
+        TextView itemSupp2 = findViewById(R.id.textViewItemSupp2);
+        TextView itemSupp3 = findViewById(R.id.textViewItemSupp3);
+
+        itemCode.setText(sc.get("ItemNo"));
+        itemDesc.setText(sc.get("Description"));
+        itemBin.setText(sc.get("Bin"));
+        itemUOM.setText(sc.get("Uom"));
+        itemSupp1.setText(sc.get("Supplier1"));
+        itemSupp2.setText(sc.get("Supplier2"));
+        itemSupp3.setText(sc.get("Supplier3"));
 
         //use adapter to pull in Txn History
 
@@ -44,8 +45,7 @@ public class StoreClerk_StockCardActivity extends Activity {
         itemStockEditBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView t1 = findViewById(R.id.textViewItemCode);
-                StationeryCatalogue sc1 = StationeryCatalogueController.searchCatalogueById(t1.getText().toString());
+                StationeryCatalogue sc1 = StationeryCatalogueController.searchCatalogueById(itemCode.getText().toString());
                 Intent intent = new Intent(getApplicationContext(),StoreClerk_EditStockQtyActivity.class);
                 intent.putExtra("invdetails", sc1);
                 startActivity(intent);
